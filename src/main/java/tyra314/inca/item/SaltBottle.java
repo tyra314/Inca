838658398a4d3395d75ac5c31d8eb12291f36162
tyra314 <tyra_oa@icloud.com>
2019-05-30 23:32:12 +0200

    Use TranslatableComponent

--- src/main/java/tyra314/inca/item/SaltBottle.java
+++ src/main/java/tyra314/inca/item/SaltBottle.java
@@ -3,11 +3,10 @@ package tyra314.inca.item;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.item.TooltipContext;
-import net.minecraft.client.resource.language.I18n;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.World;
 
 import java.util.List;
@@ -23,6 +22,6 @@ public class SaltBottle extends BaseItem
     @Override
     public void buildTooltip(ItemStack stack, World world, List<Component> list, TooltipContext context)
     {
-        list.add(new TextComponent(I18n.translate("item.inca.salt_bottle.lore")));
+        list.add(new TranslatableComponent("item.inca.salt_bottle.lore"));
     }
 }

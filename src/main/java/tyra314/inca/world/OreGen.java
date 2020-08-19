52833693ade33aacc7ffd7ae86b11937213cbd14
tyra314 <tyra_oa@icloud.com>
2020-08-20 01:43:52 +0200

    Fixes for 1.16.2

--- src/main/java/tyra314/inca/world/OreGen.java
+++ src/main/java/tyra314/inca/world/OreGen.java
@@ -1,39 +1,102 @@
 package tyra314.inca.world;
 
-import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.decorator.Decorator;
-import net.minecraft.world.gen.decorator.DecoratorConfig;
 import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
-import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
+import tyra314.inca.IncaMod;
 import tyra314.inca.block.ModBlocks;
 
-public class OreGen {
-    public static <F extends FeatureConfig, D extends DecoratorConfig> ConfiguredFeature<?, ?> configureFeature(Feature<F> feature, F featureConfig, Decorator<D> decorator, D decoratorConfig) {
-        Feature<DecoratedFeatureConfig> feature2 = feature instanceof FlowerFeature ? Feature.DECORATED_FLOWER : Feature.DECORATED;
-        return new ConfiguredFeature(feature2, new DecoratedFeatureConfig(feature.configure(featureConfig), decorator.configure(decoratorConfig)));
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+public
+class OreGen {
+    public static
+    ConfiguredFeature registerOreFeature(Identifier id,
+                                         Block block,
+                                         int size,
+                                         int bottomOffset,
+                                         int topOffset,
+                                         int maxY,
+                                         int veins
+    ) {
+        ConfiguredFeature feature = (ConfiguredFeature) ((ConfiguredFeature) Feature.ORE.configure(
+                new OreFeatureConfig(OreFeatureConfig.Rules.BASE_STONE_OVERWORLD,
+                        block.getDefaultState(),
+                        size
+                )).decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(
+                bottomOffset,
+                topOffset,
+                maxY
+        ))).spreadHorizontally()).repeat(veins);
+
+        registerFeature(id, feature);
+
+        return feature;
     }
 
-    public static void init() {
-        Registry.BIOME.forEach(biome -> handleBiome(biome));
-        RegistryEntryAddedCallback.event(Registry.BIOME).register((i, identifier, biome) -> handleBiome(biome));
-    }
+    public static
+    void init() {
+        ConfiguredFeature nautilus_ore_feature = registerOreFeature(new Identifier(
+                        IncaMod.MOD_ID,
+                        "nautilus_ore_feature"
+                ),
+                ModBlocks.NAUTILUS_ORE,
+                3,
+                0,
+                0,
+                25,
+                5
+        );
 
-    private static void handleBiome(Biome biome) {
-        if (biome.getCategory() != Biome.Category.NETHER && biome.getCategory() != Biome.Category.THEEND) {
-            biome.addFeature(
+        for (Biome biome : BuiltinRegistries.BIOME) {
+            convertImmutableFeatures(biome);
+            addFeatureToBiome(biome,
                     GenerationStep.Feature.UNDERGROUND_ORES,
-                    configureFeature(
-                            Feature.ORE,
-                            new OreFeatureConfig(
-                                    OreFeatureConfig.Target.NATURAL_STONE,
-                                    ModBlocks.NAUTILUS_ORE.getDefaultState(),
-                                    4 //Ore vein size
-                            ),
-                            Decorator.COUNT_RANGE,
-                            new RangeDecoratorConfig(1, 0, 75, 120)));
+                    nautilus_ore_feature
+            );
+        }
+    }
+
+    public static
+    void registerFeature(Identifier id, ConfiguredFeature feature) {
+        Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, id, feature);
+    }
+
+    public static
+    void addFeatureToBiome(Biome biome,
+                           GenerationStep.Feature feature,
+                           ConfiguredFeature<?, ?> configuredFeature
+    ) {
+        convertImmutableFeatures(biome);
+        List<List<Supplier<ConfiguredFeature<?, ?>>>> biomeFeatures = biome
+                .getGenerationSettings().features;
+        while (biomeFeatures.size() <= feature.ordinal()) {
+            biomeFeatures.add(Lists.newArrayList());
+        }
+        biomeFeatures.get(feature.ordinal()).add(() -> configuredFeature);
+    }
+
+    private static
+    void convertImmutableFeatures(Biome biome) {
+        if (biome.getGenerationSettings().features instanceof ImmutableList) {
+            biome.getGenerationSettings().features = biome.getGenerationSettings().features.stream()
+                                                                                           .map(Lists::newArrayList)
+                                                                                           .collect(
+                                                                                                   Collectors
+                                                                                                           .toList());
         }
     }
 
